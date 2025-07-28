69ba75bb3cac72e7e6291d86dd4e8177a34363b1
Tufan Butuner <43154254+tufanbutuner@users.noreply.github.com>
2025-07-28 12:06:09 +0100

    Fix StatusTag storybook types error

--- src/stories/StatusTag.stories.tsx
+++ src/stories/StatusTag.stories.tsx
@@ -1,39 +1,40 @@
-// @ts-nocheck ignore type checking for this file
-import { StatusTag } from "../components/StatusTagComponent/StatusTag";
+import StatusTagComponent from "../components/StatusTagComponent/StatusTagComponent";
+
+type Props = { status: string };
 
 export default {
   title: "Components/StatusTag",
-  component: <StatusTag />,
+  component: StatusTagComponent,
   argTypes: {
     status: { control: "text" },
   },
 };
 
-export const Unused = (args) => <status-tag {...args} />;
+export const Unused = (args: Props) => <status-tag {...args} />;
 Unused.args = {
   status: "Unused",
 };
 
-export const Used = (args) => <status-tag {...args} />;
+export const Used = (args: Props) => <status-tag {...args} />;
 Used.args = {
   status: "Used",
 };
-export const None = (args) => <status-tag {...args} />;
+export const None = (args: Props) => <status-tag {...args} />;
 None.args = {
   status: "None",
 };
 
-export const Reclassified = (args) => <status-tag {...args} />;
+export const Reclassified = (args: Props) => <status-tag {...args} />;
 Reclassified.args = {
   status: "Reclassified",
 };
 
-export const Renamed = (args) => <status-tag {...args} />;
+export const Renamed = (args: Props) => <status-tag {...args} />;
 Renamed.args = {
   status: "Renamed",
 };
 
-export const New = (args) => <status-tag {...args} />;
+export const New = (args: Props) => <status-tag {...args} />;
 New.args = {
   status: "New",
 };
--- src/types/status-tag.d.ts
+++ src/types/status-tag.d.ts
@@ -1,12 +1,12 @@
 declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      "status-tag": React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement> & {
-          status?: string;
-        },
-        HTMLElement
-      >;
+  namespace React {
+    namespace JSX {
+      interface IntrinsicElements {
+        "status-tag": React.DetailedHTMLProps<
+          React.HTMLAttributes<HTMLElement>,
+          HTMLElement
+        > & { status?: string };
+      }
     }
   }
 }
