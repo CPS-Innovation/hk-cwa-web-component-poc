525fb13d067e7f99236eaf0053e6933bf5f28296
James Chapman <james.chapman@cps.gov.uk>
2025-07-29 11:20:29 +0100

    Rename banner to notification banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-// @ts-nocheck
 import "./App.css";
 import "./components/StatusTagComponent/StatusTagComponent";
-import "./components/BannerComponent/BannerComponent";
+import "./components/NotificationBannerComponent/NotificationBannerComponent.tsx";
 import "./components/CaseInfoSummaryComponent/CaseInfoSummaryComponent";
 
 function App() {
   return (
     <div className="App">
-      <case-info-summary-component />
+      <case-info-summary />
 
       <status-tag status="Renamed" />
 
-      <banner-component
+      <notification-banner
         type="success"
         header="Success Header"
         content="Success Content"
